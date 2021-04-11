4791538e3efd1a943c538805f495b9aa38431b2b
sureshvenkey <50266914+sureshvenkey@users.noreply.github.com>
2021-04-11 20:56:07 +0530

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -1,14 +1,15 @@
-cat /etc/redhat-release
-dnf update -y
-dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
-dnf repolist -v
-dnf list docker-ce
-dnf install docker-ce-3:18.09.1-3.el7
-systemctl disable firewalld
-systemctl enable --now docker
-systemctl start docker
-systemctl is-active docker
-docker --version
-curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-docker-compose --version
+#!/bin/bash
+sudo cat /etc/redhat-release
+sudo dnf update -y
+sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+sudo dnf repolist -v
+sudo dnf list docker-ce
+sudo dnf install docker-ce-3:18.09.1-3.el7
+sudo systemctl disable firewalld
+sudo systemctl enable --now docker
+sudo systemctl start docker
+sudo systemctl is-active docker
+sudo docker --version
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo docker-compose --version
