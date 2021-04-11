b83d78b6e88ef986608fda12169c18c991c8c060
sureshvenkey <50266914+sureshvenkey@users.noreply.github.com>
2021-04-11 21:59:15 +0530

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -4,7 +4,7 @@ sudo dnf update -y
 sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
 sudo dnf repolist -v
 sudo dnf list docker-ce
-sudo dnf install docker-ce-3:18.09.1-3.el7
+sudo dnf install docker-ce -y
 sudo systemctl disable firewalld
 sudo systemctl enable --now docker
 sudo systemctl start docker
@@ -12,4 +12,4 @@ sudo systemctl is-active docker
 sudo docker --version
 sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-sudo docker-compose --version
+sudo /usr/local/bin/docker-compose --version
